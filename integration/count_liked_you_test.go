78cb528f858489855eccf5d2dd2cd77f57b3ccc4
yakob-abada <jakob@gymcatch.com>
2024-12-12 23:20:06 +0000

    add more integration test

---
+++ integration/count_liked_you_test.go
@@ -0,0 +1,42 @@
+package integration
+
+import (
+	"context"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	pb "github.com/yakob-abada/backend-match/explore/proto"
+	"google.golang.org/grpc"
+	"log"
+	"testing"
+)
+
+func TestCountLikedYou(t *testing.T) {
+	conn, err := grpc.Dial("localhost:9001", grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+
+	defer conn.Close()
+
+	fmt.Println("Connected!")
+
+	c := pb.NewExploreServiceClient(conn)
+	t.Run("success", func(t *testing.T) {
+		response, err := c.CountLikedYou(context.TODO(), &pb.CountLikedYouRequest{
+			RecipientUserId: "1",
+		})
+
+		assert.Nil(t, err)
+		assert.Equal(t, uint64(7), response.Count)
+	})
+
+	t.Run("success no match for given user", func(t *testing.T) {
+		response, err := c.CountLikedYou(context.TODO(), &pb.CountLikedYouRequest{
+			RecipientUserId: "2",
+		})
+
+		assert.Nil(t, err)
+		assert.Equal(t, uint64(0), response.Count)
+	})
+}
